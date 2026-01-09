a0c6f1819c05777a902a06a2f099494320c04877
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2026-01-09 13:56:54 +0500

    first commit

---
+++ shop/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50 , null=False)
+    description = models.TextField(max_length=100, null=False)
+    slug = models.SlugField(max_length=20, null=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+
+
+    def __str__(self):
+        return self.name
