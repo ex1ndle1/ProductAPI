99779b6a724100b569eab615c07b4cffeb8bdb18
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2026-01-09 14:07:54 +0500

    .

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
-
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, ProductListByChildCategorySlug
 
 app_name = 'shop'
-
+router = DefaultRouter()
+router.register('products', ProductViewSet, basename='product')
 
 urlpatterns = [ 
-    
+      path('products/category/<slug:slug>/', ProductListByChildCategorySlug.as_view(), name='products-by-category'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .serialazers import ProductSerializer
+from .models import Product
 # Create your views here.
+from rest_framework import viewsets, generics           
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductListByChildCategorySlug(generics.ListAPIView):
+    serializer_class = ProductSerializer
+    def get_queryset(self):
+        category_slug = self.kwargs['slug']        
+        return Product.objects.filter(category__slug=category_slug)
